d5e3c0eedb007595b228019919c6e757d619e272
Bill Flores <billflores@Bills-iMac.local>
2019-05-14 14:48:15 -0500

    add polymorphism

--- app/src/main/java/com/flores/pooandroid/MainActivity.kt
+++ app/src/main/java/com/flores/pooandroid/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flores.pooandroid.sampleAbstract.AbstractActivity
+import com.flores.pooandroid.sampleEncapsulation.EncapsulationActivity
 import com.flores.pooandroid.sampleInheritance.InheritanceActivity
+import com.flores.pooandroid.samplePolymorphism.PolymorphismActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,16 +14,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         btnHeritage.setOnClickListener {
             startActivity(Intent(this, InheritanceActivity::class.java))
         }
 
         btnEncapsulation.setOnClickListener {
-
+            startActivity(Intent(this, EncapsulationActivity::class.java))
         }
 
         btnAbstract.setOnClickListener {
             startActivity(Intent(this, AbstractActivity::class.java))
+        }
+
+        btnPolymorphism.setOnClickListener {
+            startActivity(Intent(this, PolymorphismActivity::class.java))
         }
 
     }
---
+++ app/src/main/java/com/flores/pooandroid/samplePolymorphism/PolymorphismActivity.kt
@@ -0,0 +1,13 @@
+package com.flores.pooandroid.samplePolymorphism
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.flores.pooandroid.R
+
+class PolymorphismActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_polymorphism)
+    }
+}
