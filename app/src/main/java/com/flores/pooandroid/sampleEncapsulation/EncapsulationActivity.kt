690f054a6dbe04025538a762c240e6cab86c265d
Bill Flores <billflores@Bills-iMac.local>
2019-05-14 14:24:50 -0500

    finish sample Encapsulation

--- app/src/main/java/com/flores/pooandroid/sampleEncapsulation/EncapsulationActivity.kt
+++ app/src/main/java/com/flores/pooandroid/sampleEncapsulation/EncapsulationActivity.kt
@@ -3,6 +3,7 @@ package com.flores.pooandroid.sampleEncapsulation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.flores.pooandroid.R
+import kotlinx.android.synthetic.main.activity_encapsulation.*
 
 class EncapsulationActivity : AppCompatActivity() {
 
@@ -10,11 +11,17 @@ class EncapsulationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_encapsulation)
 
-        val examNotes = ExamNotes()
-        //examNotes.note=2.3 -- incorrect
-        examNotes.setNotes(2.3)
-        examNotes.getNoteOfExam()
-        examNotes.exam = "nombre de examen"
+        val examNotes = ExamNotes("nombre de examen")
 
+        //examNotes.note = 2.3 -- incorrect
+        examNotes.setNotes(2.3)
+
+        //examNotes.namePerson = "Juancho" -- incorrect
+        examNotes.setNamePerson("Juancho")
+
+        tvDetails.text = examNotes.getExam() + "\n" +
+                examNotes.getNamePerson() + "\n" +
+                examNotes.getNoteOfExam()
     }
+
 }
--- app/src/main/java/com/flores/pooandroid/sampleEncapsulation/ExamNotes.kt
+++ app/src/main/java/com/flores/pooandroid/sampleEncapsulation/ExamNotes.kt
@@ -2,9 +2,17 @@ package com.flores.pooandroid.sampleEncapsulation
 
 class ExamNotes {
 
+    private var exam: String = ""
     private var note: Double = 0.0
-    var exam: String = ""
     private var namePerson: String = ""
+
+    constructor(exam: String) {
+        this.exam = exam
+    }
+
+    fun getExam(): String {
+        return exam
+    }
 
     fun setNotes(note: Double) {
         this.note = if (note < 0) 0.0 else note
@@ -12,6 +20,14 @@ class ExamNotes {
 
     fun getNoteOfExam(): Double {
         return note
+    }
+
+    fun setNamePerson(namePerson: String) {
+        this.namePerson = namePerson
+    }
+
+    fun getNamePerson(): String {
+        return namePerson
     }
 
 }
